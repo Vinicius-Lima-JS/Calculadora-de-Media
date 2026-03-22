f628d59a22e1c4905d19cc5c379e83e5ee6289dd
Vinicius-Lima-JS <infoarquives@gmail.com>
2026-03-22 13:35:46 -0300

    Create calculador.c

---
+++ calculador.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+
+{
+    float nota1, nota2, media;
+    printf("Entre com a nota 1: ");
+    scanf("%f",&nota1);
+    printf("Entre com a nota 2: ");
+    scanf("%f",&nota2);
+
+    media = (nota1 + nota2) / 2;
+
+    printf("Sua média é = %.2f\n\n",media);
+
+    if(media >= 6)
+    {
+        printf("Aprovado\n\n");
+    }
+
+    else
+    {
+        printf("Reprovado\n\n");
+
+    }
+}
